bb859cfcb2df8e5d0ead8af30c2000d727da14c9
bharat <bharat@amwebtech.com>
2023-06-06 17:10:32 +0530

    fixed issue

--- node-api/core/db.js
+++ node-api/core/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 exports.createDBConnection = () => {
   mongoose.Promise = global.Promise;
   // const connect = mongoose.connect("mongodb://0.0.0.0:27017/mean-zoom-clone", {
-  const connect = mongoose.connect("mongodb+srv://bharat:EqnwUpI5RVJufWTy@cluster0.i3fkd.mongodb.net/mean-zoom-clone?retryWrites=true&w=majority", {
+  const connect = mongoose.connect("mongodb+srv://bharat:EqnwUpI5RVJufWTy@cluster0.i3fkd.mongodb.net/amw-zoom?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true,
