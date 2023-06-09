7f82099fe913f5ff95c16245a789d4750b9a3b83
rajat <rajat@amwebtech.com>
2023-06-09 16:46:13 +0530

    working on delete meeting from andular-ui

--- angular-ui/src/app/shared-ui/services/meetings.service.ts
+++ angular-ui/src/app/shared-ui/services/meetings.service.ts
@@ -45,6 +45,14 @@ export class MeetingsService {
     );
   }
 
+  public deleteMeeting(param: object): Observable<any> {
+    return this.apiService.delete(`${this.meetings}/deleteMeeting`, param).pipe(
+      map((data) => {
+        return data;
+      })
+    );
+  }
+
   public getMeetingsList(): Observable<any> {
     return this.apiService.post(`${this.meetings}/getMeetingsList`).pipe(
       map((data) => {
