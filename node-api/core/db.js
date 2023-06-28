7836c0157a759314e287411ba330684837387a19
bharat <bharat@amwebtech.com>
2023-06-28 10:10:19 +0530

    fixed shared screen option

--- node-api/core/db.js
+++ node-api/core/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 exports.createDBConnection = () => {
   mongoose.Promise = global.Promise;
-  const connect = mongoose.connect("mongodb://0.0.0.0:27017/amw-meet-clone", {
-    // const connect = mongoose.connect("mongodb+srv://bharat:EqnwUpI5RVJufWTy@cluster0.i3fkd.mongodb.net/amw-zoom?retryWrites=true&w=majority", {
+  // const connect = mongoose.connect("mongodb://0.0.0.0:27017/amw-meet-clone", {
+  const connect = mongoose.connect("mongodb+srv://bharat:EqnwUpI5RVJufWTy@cluster0.i3fkd.mongodb.net/amw-zoom?retryWrites=true&w=majority", {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
     // useCreateIndex: true,
